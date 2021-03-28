53846c83484e7d8779333a7187d42bfa272793db
Ryan Tosh <42220798+RedwolfPrograms@users.noreply.github.com>
2021-03-28 14:45:39 -0500

    Create chat-monospace-fix.user.js

---
+++ chat-monospace-fix.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Chat Redesign
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Makes chat look more like main
+// @author       Redwolf Programs
+// @match        https://chat.stackexchange.com/rooms/240/the-nineteenth-byte
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var style = document.createElement("style");
+
+    document.head.appendChild(style);
+
+    var sheet = style.sheet;
+
+    sheet.insertRule([
+        ".content code {",
+        "font-family: Consolas, Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, sans-serif;",
+        "background-color: rgb(228, 230, 232);",
+        "padding: 1px 5px",
+        "}"
+    ].join("\n"));
+})();
