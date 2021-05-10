f59ff9037a8b10ccb36564daccbdbf190410a07a
Ryan Tosh <42220798+RedwolfPrograms@users.noreply.github.com>
2021-05-10 09:34:12 -0500

    Update custom-fonts.user.js

--- custom-fonts.user.js
+++ custom-fonts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Custom Fonts
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Allows customization of the fonts on Stack Exchange
 // @author       Redwolf Programs
 // @match        https://stackexchange.com/*
@@ -17,9 +17,12 @@
 // ==/UserScript==
 
 (() => {
+    var Imports = ""; // "@import { /* I don't know how imports work */ }"
+
     var Rules = [
         {
             site: "https:", // Apply to all sites
+            mono: "\"Comic Sans MS\", \"Comic Sans\", cursive"
 
             // sans: "Arial, \"Helvetica Neue\", Helvetica, sans-serif",
             // mono: "Consolas, Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, sans-serif"
@@ -59,12 +62,15 @@
 
     var style = document.createElement("style");
 
-    style.textContent = ("body{ " +
+    style.textContent = ("body { " +
         ([
             ["--ff-sans", sans], ["--ff-serif", serif], ["--ff-mono", mono], ["--theme-body-font-family", theme]
         ].filter(r => r[1]).map(r => r[0] + ": " + r[1] + " !important; ")).join("") +
         "--theme-question-body-font-family: var(--theme-body-font-family) !important; " +
         "--theme-question-title-font-family: var(--theme-body-font-family) !important; " +
+        "} " +
+        "pre, code { " +
+        "font-family: var(--ff-mono) !important; " +
         "}"
     );
 
