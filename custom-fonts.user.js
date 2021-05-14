3712690ad381c25a553f556fa551626d48fbc0c0
Ryan Tosh <42220798+RedwolfPrograms@users.noreply.github.com>
2021-05-14 17:26:29 -0500

    Update custom-fonts.user.js

--- custom-fonts.user.js
+++ custom-fonts.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Custom Fonts
 // @namespace    http://tampermonkey.net/
-// @version      1.2.1
+// @version      1.2.2
 // @description  Allows customization of the fonts on Stack Exchange
 // @author       Redwolf Programs
+// @match        https://stackexchange.com
 // @match        https://stackexchange.com/*
 // @match        https://*.stackexchange.com/*
 // @match        https://stackoverflow.com/*
@@ -29,8 +30,8 @@
         {
             site: "https:", // Apply to all sites
 
-            // sans: "Arial, \"Helvetica Neue\", Helvetica, sans-serif",
-            // mono: "Consolas, Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, sans-serif"
+            sans: "Roboto, Arial, \"Helvetica Neue\", Helvetica, sans-serif",
+            mono: "Roboto Mono, Consolas, Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, sans-serif"
         }
     ];
 
@@ -74,6 +75,7 @@
         ].filter(r => r[1]).map(r => r[0] + ": " + r[1] + " !important; ")).join("") +
         "--theme-question-body-font-family: var(--theme-body-font-family) !important; " +
         "--theme-question-title-font-family: var(--theme-body-font-family) !important; " +
+        "font-family: var(--theme-body-font-family) !important; " +
         "} " +
         [
             "body.channels-page .top-bar",
@@ -84,15 +86,17 @@
             ".top-bar", "#user-menu", ".popup-close a", ".message.message-error .message-close",
             ".message.message-info .message-close", ".message.message-warning .message-close",
             ".message.message-config .message-close,.message.message-info.contributor-dropdown .message-close",
-            ".message.message-success .message-close"
+            ".message.message-success .message-close", ".answerBox .numAnswer", ".hotness-rank", ".statsBox .number",
+            ".inputField", "input[type=submit],input[type=button],.btn", ".page-numbers", ".ac_results li", ".ac_over",
+            ".siteSelect form select", ".statsBox .statsLink", ".sort a,.subtabs a", ".unreadCount", ".unreadCountTab",
+            ".top-question-score-number", ".bookmark-number", ".account-container .account-stat .account-number",
+            ".lv-stats-box .number", "#noscript-warning"
         ].map(c => "html " + c + " { font-family: var(--ff-sans) !important; }").join(" ") +
         [
             "pre.s-code-block", ".s-prose code", "pre", "code", ".CodeMirror",
             "textarea.wmd-input,textarea#wmd-input", ".full-diff .content"
         ].map(c => "html " + c + " { font-family: var(--ff-mono) !important; }").join(" ")
     );
-    
-    console.log(style.textContent);
 
     try {
         document.head.appendChild(style);
